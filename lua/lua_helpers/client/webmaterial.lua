4f2334b592a7499cedf9c3f45200e9c96fb246a1
Tenrys <tenrys.iaido@live.fr>
2017-11-10 18:21:53 +0100

    fix

--- lua/lua_helpers/client/webmaterial.lua
+++ lua/lua_helpers/client/webmaterial.lua
@@ -6,7 +6,7 @@ if not file.IsDir(tag, "DATA") then
 end
 
 local cache = {}
-local function FetchWebMaterial(name, url, callback)
+local function FetchWebMaterial(name, url)
 	http.Fetch(url, function(data, len, _, err)
 		file.Write(tag .. "/" .. name .. ".png", data)
 
@@ -18,10 +18,6 @@ local function FetchWebMaterial(name, url, callback)
 		end
 
 		cache[name] = mat
-
-		if isfunction(callback) then
-			callback(name, url, mat)
-		end
 	end, function(err)
 		Msg"[WebMaterial] "print("HTTP fetch failed for ", url, ": " .. tostring(err))
 	end)
@@ -30,19 +26,22 @@ end
 local fallback = Material("models/debug/debugwhite")
 function WebMaterial(name, url, callback)
 	if not file.Exists(tag .. "/" .. name .. ".png", "DATA") then
-		FetchWebMaterial(name, url, callback)
+		FetchWebMaterial(name, url)
 	else
 		local mat = Material("../data/" .. tag .. "/" .. name .. ".png")
 
 		if not mat or mat:IsError() then
 			Msg"[WebMaterial] "print("Material found, but it's an error: ", name, ", redownloading")
-			FetchWebMaterial(name, url, callback)
+			FetchWebMaterial(name, url)
 		else
 			cache[name] = mat
 		end
 	end
 	local c = cache[name]
 	c = (not c or c:IsError()) and fallback or c
+	if isfunction(callback) then
+		callback(name, url, c)
+	end
 	return c or fallback
 end
 
